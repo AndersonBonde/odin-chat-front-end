bd411219347ee545d21eabd8a84383e64857c42a
Anderson Bonde <andersonbonde@gmail.com>
2025-04-28 14:00:14 -0300

    Add functionality to POST messages in general chat

--- src/scripts/addEventListeners.js
+++ src/scripts/addEventListeners.js
@@ -2,10 +2,31 @@ const chatForm = document.getElementById('chat-typing-box');
 
 chatForm.addEventListener('submit', async (e) => {
   e.preventDefault();
-
-  const user = JSON.parse(localStorage.getItem('token'));
-
+  
   const text = document.getElementById('chat-textarea').value;
-  const author = user ? user.email : localStorage.getItem('guest');
-  console.log(author, text);
+  if (text.length < 1) return;
+
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const id = user?.id;
+  const guestName = localStorage.getItem('guest') || null;
+
+  try {
+    const res = await fetch('http://localhost:3000/messages/1', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id, text, guestName }),
+    });
+    if (!res.ok) throw new Error(`Failed to POST message, Status: ${res.status}`);
+  
+    const data = await res.json();
+    console.log(data.message, data.newMessage);
+
+    window.location.href = './index.html'; // PLACEHOLDER
+    
+  } catch (err) {
+    console.error('Failed to POST message', err);
+  }
+
+  chatForm.reset();
 });
