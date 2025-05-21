d01dcaa8082e28d1bc9f1338023a0066d7f3d603
Anderson Bonde <andersonbonde@gmail.com>
2025-05-21 14:13:17 -0300

    roomList scaffolding

--- src/scripts/populateRoomList.js
+++ src/scripts/populateRoomList.js
@@ -1,3 +1,25 @@
 const roomList = document.querySelector('#room-list');
 
+async function fetchChatRooms() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const token = localStorage.getItem('token');
+  
+  if (!user || !token) return;
 
+  try {
+    const res = await fetch(`http://localhost:3000/users/chat-rooms/${user.id}`, {
+      method: 'GET',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      }
+    });
+
+    const data = await res.json();
+
+    return data;
+
+  } catch (err) {
+    console.error('Failed to fetch chat rooms list');
+  }
+}
